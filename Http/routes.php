abbc9404ab06e4eb8fe7010a73ac97caa1325e6a
Hilkiah Makemo <hilmak01@gmail.com>
2018-09-21 18:40:09 -0600

    Initial push from local

---
+++ Http/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+foreach(['my', 'admin'] as $prefix){
+
+    Route::group([
+        'prefix'        => $prefix,
+        'middleware'    => ['web', 'Admin'],
+        'namespace'     => 'Admin\Dashboard\Http\Controllers'],
+        function(){
+            Route::resource('dashboard', DashboardController::class);
+        });
+}
