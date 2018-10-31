ed6ba663c66e316d61cdafeb8bdedf6cd545168f
Hilkiah Makemo <hilmak01@gmail.com>
2018-10-31 16:55:25 -0600

    Added new portal views

---
+++ Resources/views/content/employee.blade.php
@@ -0,0 +1,58 @@
+
+<ul class="list-group">
+    <li class="list-group-item">
+        Conditional Offer of Employment
+        <br/>
+       <span class="text-success">
+           Status: Complete 03/10/{{ date('Y')}}
+       </span>
+    </li>
+    <li class="list-group-item">Background Check
+        <br/>
+        <span class="text-success">
+       Status: Complete 03/10/{{ date('Y')}}
+        </span>
+    </li>
+    <li class="list-group-item">Confidentiality and Non Disclosure Agreement
+        <br/>
+        <span class="text-success">
+       Status: Complete 03/10/{{ date('Y')}}
+        </span>
+    </li>
+    <li class="list-group-item">Driver License/ID
+        <br/>
+        <span class="text-success">
+       Status: Complete 03/12/{{ date('Y')}}
+        </span>
+    </li>
+    <li class="list-group-item">Social Security Card/TIN
+        <br/>
+        <span class="text-success">
+       Status: Complete 03/12/{{ date('Y')}}
+        </span>
+    </li>
+    <li class="list-group-item">Substance Abuse Program Ack
+        <br/>
+        <span class="text-success">
+       Status: Complete 03/10/{{ date('Y')}}
+        </span>
+    </li>
+    <li class="list-group-item">Company Policy Manual Acknowledgement
+        <br/>
+        <span class="text-success">
+       Status: Complete 03/10/{{ date('Y')}}
+        </span>
+    </li>
+    <li class="list-group-item">Employment Eligibility Form (I-9)
+        <br/>
+        <span class="text-warning">
+            Status: Pending Review
+        </span>
+    </li>
+    <li class="list-group-item">Work Oportunity Tax Credit (WOTC)
+        <br/>
+        <span class="text-danger">
+            Status: <a href="#">Not started </a>
+        </span>
+    </li>
+</ul>
